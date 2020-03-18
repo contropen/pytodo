4e0906d6e9c5aa13da78ee2c8c1e3dce52a1f281
Mugdha Dalal <mugdhadalal@example.com>
2020-03-18 02:53:46 -0400

    changes made

--- pytodo/database.py
+++ pytodo/database.py
@@ -3,20 +3,57 @@ import sqlite3 #library for database
 class DatabaseConnection:
  
     def __init__(self):
-        self.con = sqlite3.connect("generated_data.db") 
+        self.con = sqlite3.connect("generated_todo.db") 
+        self.cursor_obj = self.con.cursor()
+        self.cursor_obj.execute(
+            """CREATE TABLE IF NOT EXISTS data(
+            task_name text NOT NULL UNIQUE, 
+            creation_date varchar, task varchar)
+            """
+        )
         self.con.commit()
-    def insert_data(self, portal_name, creation_date):
+
+    def insert_data(self, task_name, data, creation_date):
         self.creation_date = creation_date
-        self.portal_name = portal_name
+        self.task_name = task_name
+        self.data = data
+        self.cursor_obj.execute(
+            """INSERT INTO data
+            (task_name, data, creation_date)
+            VALUES (?, ?, ?)""",
+            (self.task_name, self.data, self.creation_date),
+        )
         self.con.commit()
-    def delete_data(self, portal_name):
-        self.portal_name = portal_name
+
+    def delete_data(self, task_name):
+        self.task_name = task_name
+        self.cursor_obj.execute(
+            """DELETE from data where task_name = ?""", (self.task_name,)
+        )
         self.con.commit()
-    def show_data(self, portal_name):
-        self.portal_name = portal_name
+
+    def show_data(self, task_name):
+        self.task_name = task_name
+        self.cursor_obj.execute(
+            """SELECT data FROM datas WHERE task_name=?""", (self.task_name,)
+        )
+        rows = self.cursor_obj.fetchall()
+
+        for row in rows:
+            return row[0]
+    
         self.con.commit()
-    def update_data(self,portal_name):
-        self.portal_name = portal_name
+
+    def update_data(self,task_name):
+        self.task_name = task_name
+        self.cursor_obj.execute(
+            """UPDATE data  =? WHERE task_name =?""",
+            (self.data, self.task_name),
+        )
         self.con.commit()
+
     def show_all_data(self):
+        self.cursor_obj.execute("""SELECT * FROM data""")
+        rows = self.cursor_obj.fetchall()
+        return rows
         self.con.commit()
--- pytodo/todo.py
+++ pytodo/todo.py
@@ -1,58 +1,73 @@
 import click
 from datetime import date
+from beautifultable import BeautifulTable
 
 from pytodo.database import DatabaseConnection
 
 db_obj = DatabaseConnection()
+table = BeautifulTable()
+table.left_border_char = "|"
+table.right_border_char = "|"
+table.top_border_char = "="
+table.header_separator_char = "="
+table.column_headers = ["task_name_", "data", "date"]
+
 
 @click.command(help="show all data")
 def showall():
     show_all = db_obj.show_all_data()
     if show_all == []:
         print("Empty")
+    for row in show_all:
+        table.append_row([row[0], row[1], row[2]])
+    print(table)
     
 @click.command(help="add data")
 def add():
-    portal_name = click.prompt("Enter the portal name")
-    portal_name = click.prompt("Enter data")
+    task_name = click.prompt("Enter the task name")
+
+    data = click.prompt("Enter the data")
     creation_date = date.today()
     db_obj.insert_data(
-        portal_name=portal_name,
+        task_name=task_name,
         creation_date = creation_date,
+        data=data,
     )    
 
 @click.command(help="delete data")
 def delete():
-    portal_name = click.prompt("Enter data name", default="None")
-    value_check = db_obj.show_data(portal_name)
+    task_name = click.prompt("Enter data name", default="None")
+    value_check = db_obj.show_data(task_name)
     if value_check is None:
         print("No records found")
     else:
-        db_obj.delete_data(portal_name=portal_name)
+        db_obj.delete_data(task_name=task_name)
 
 @click.command(help="update the data")
 def update():
-    portal_name = click.prompt("Enter data name", default="None")
-    mod_check = db_obj.show_data(portal_name)
+    task_name = click.prompt("Enter data name", default="None")
+    mod_check = db_obj.show_data(task_name)
     if mod_check is None:
         print("No records found")
     else:
         mod = click.prompt("Enter new data", default="None", hide_input=True)
-        db_obj.update_data(portal_name=portal_name, data=mod)
+        db_obj.update_data(task_name=task_name, data=mod)
 
 @click.command(help="save existing data")
 def savedata():
-    portal_name = click.prompt("Enter portal name",default='None')
+    task_name = click.prompt("Enter task name",default='None')
+    dta = click.prompt("Enter your data", default="None")
     creation_date = date.today()
     db_obj.insert_data(
-        portal_name=portal_name,
+        task_name=task_name,
+        data=dta,
         creation_date=creation_date,
     )
 
 @click.command(help="Show data")
 def showdata():
-    portal_name = click.prompt("Enter portal name", default="None")
-    spass = db_obj.show_data(portal_name)
+    task_name = click.prompt("Enter task name", default="None")
+    spass = db_obj.show_data(task_name)
     if spass is None:
         print("No records found")
     else:
